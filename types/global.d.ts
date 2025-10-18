1f5a87b6b3aa4f917b9ca9418ee1fab4d907ccd0
NavvAbhishek <abi2000navi@gmail.com>
2025-10-18 23:52:23 +0530

    feat: implement sign-up form

---
+++ types/global.d.ts
@@ -0,0 +1,41 @@
+declare global {
+    type SignUpFormData = {
+        fullName: string;
+        email: string;
+        password: string;
+        country: string;
+        investmentGoals: string;
+        riskTolerance: string;
+        preferredIndustry: string;
+    };
+
+      type FormInputProps = {
+        name: string;
+        label: string;
+        placeholder: string;
+        type?: string;
+        register: UseFormRegister;
+        error?: FieldError;
+        validation?: RegisterOptions;
+        disabled?: boolean;
+        value?: string;
+    };
+
+       type SelectFieldProps = {
+        name: string;
+        label: string;
+        placeholder: string;
+        options: readonly Option[];
+        control: Control;
+        error?: FieldError;
+        required?: boolean;
+    };
+
+    type FooterLinkProps = {
+        text: string;
+        linkText: string;
+        href: string;
+    };
+}
+
+export { };
